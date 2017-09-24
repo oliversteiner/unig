16046bd35e8050451d0f9652b893d5bd12bef34f
Oliver Steiner <oliver@mollo.ch>
2017-09-24 09:24:25 +0200

    album name

--- src/Utility/FileTrait.php
+++ src/Utility/FileTrait.php
@@ -2,157 +2,173 @@
 
 namespace Drupal\unig\Utility;
 
-use \Drupal\file\Entity\File;
+use Drupal\file\Entity\File;
 
 
 trait FileTrait {
 
-    public $bundle_file = 'unig_file';
+  public $bundle_file = 'unig_file';
 
-    // define Extensions to be used als imagefield
-    private $ext_image = ['jpg', 'jpeg', 'gif', 'png'];
-    /**
-     * createNodeUniGImage
-     *
-     * Node Fields:
-     *      - field_unig_project: Entity
-     *      - field_unig_image : Image
-     *
-     *  Inputs Plupload:
-     *      - tmppath   => string(45) "temporary://o_1bfv2k9af2fdqogn551i9b1uqfc.tmp"
-     *      - tmpname   => string(33) "o_1bfv2k9af2fdqogn551i9b1uqfc.tmp"
-     *      - name      => string(22) "451415562631785265.jpg"
-     *      - status    => string(4) "done"
-     *
-     * @param $file_tmp
-     * @param $project_nid
-     *
-     * @return int
-     */
-    public function createNodeUniGImage($file_tmp, $project_nid)
-    {
+  // define Extensions to be used als imagefield
+  private $ext_image = ['jpg', 'jpeg', 'gif', 'png', 'svg'];
 
-        // define entity type and bundle
-        $entity_type = "node";
+  /**
+   * createNodeUniGImage
+   *
+   * Node Fields:
+   *      - field_unig_project: Entity
+   *      - field_unig_image : Image
+   *
+   *  Inputs Plupload:
+   *      - tmppath   => string(45)
+   * "temporary://o_1bfv2k9af2fdqogn551i9b1uqfc.tmp"
+   *      - tmpname   => string(33) "o_1bfv2k9af2fdqogn551i9b1uqfc.tmp"
+   *      - name      => string(22) "451415562631785265.jpg"
+   *      - status    => string(4) "done"
+   *
+   * @param $file_tmp
+   * @param $project_nid
+   *
+   * @return int
+   */
+  public function createNodeUniGImage($file_tmp, $project_nid) {
 
-        // get fid of the temporary uploaded file
-        $file_id = $this->getFileId($file_tmp, $project_nid);
+    // define entity type and bundle
+    $entity_type = "node";
 
-        // split the filename: get name and lowercase extension separately
-        $file_temp = $file_tmp['name'];
-        $file_name = pathinfo($file_temp, PATHINFO_FILENAME);
-        $file_ext = strtolower(pathinfo($file_temp, PATHINFO_EXTENSION));
+    // get fid of the temporary uploaded file
+    $file_id = $this->getFileId($file_tmp, $project_nid);
 
-
-        // Node Title is filename without file extension
-        $node_title = $file_name;
-
-        // Node Body is empty
-        $node_body = '';
-
-        // get definition of target entity type
-        $entity_def = \Drupal::EntityTypeManager()->getDefinition($entity_type);
-
-        // load up an array for creation
-        $new_node = array(
-            'title' => $node_title,
-            'body' => $node_body,
-            $entity_def->get('entity_keys')['bundle'] => $this->bundle_file,
-        );
-
-        // Init new node
-        $new_post = \Drupal::EntityTypeManager()->getStorage($entity_type)->create($new_node);
-
-        //
-
-        $new_post->field_unig_project->setValue([
-            'target_id' => $project_nid]);
-
-        // check file if Image or File:
-        if (in_array($file_ext,$this->ext_image )){
-
-            // if Image save to Imagefield
-            $new_post->field_unig_image->setValue([
-                'target_id' => $file_id]);
-        }
-        else{
-
-            // if other save for Filefield
-
-        }
+    // split the filename: get name and lowercase extension separately
+    $file_temp = $file_tmp['name'];
+    $file_name = pathinfo($file_temp, PATHINFO_FILENAME);
+    $file_ext = strtolower(pathinfo($file_temp, PATHINFO_EXTENSION));
 
 
-        $new_post->save();
+    // Node Title is filename without file extension
+    $node_title = $file_name;
 
-        // hole die neu erstellte ID
-        $new_node_id = $new_post->id();
+    // Node Body is empty
+    $node_body = '';
 
-        return $new_node_id;
+    // get definition of target entity type
+    $entity_def = \Drupal::EntityTypeManager()->getDefinition($entity_type);
+
+    // load up an array for creation
+    $new_node = [
+      'title' => $node_title,
+      'body' => $node_body,
+      $entity_def->get('entity_keys')['bundle'] => $this->bundle_file,
+    ];
+
+    // Init new node
+    $new_post = \Drupal::EntityTypeManager()
+      ->getStorage($entity_type)
+      ->create($new_node);
+
+    //
+
+    $new_post->field_unig_project->setValue([
+      'target_id' => $project_nid,
+    ]);
+
+    // check file if Image or File:
+    if (in_array($file_ext, $this->ext_image)) {
+
+      // if Image save to Imagefield
+      $new_post->field_unig_image->setValue([
+        'target_id' => $file_id,
+      ]);
+    }
+    else {
+
+      // if other save for Filefield
+
     }
 
 
-    /**
-     * @param $values
-     * @return array
-     */
-    public function createMultiNode($values)
-    {
+    $new_post->save();
 
-        // Nodes erstellen
-        $node_ids = [];
-        $file_upload = $values['file_upload'];
-        $project_nid = $values['project_nid'];
+    // hole die neu erstellte ID
+    $new_node_id = $new_post->id();
 
-        foreach ($file_upload as $file_tmp) {
-            $node_ids[] = $this->createNodeUniGImage($file_tmp, $project_nid);
-        }
+    return $new_node_id;
+  }
 
-        return $node_ids;
+
+  /**
+   * @param $values
+   *
+   * @return array
+   */
+  public function createMultiNode($values) {
+
+    // Nodes erstellen
+    $node_ids = [];
+    $file_upload = $values['file_upload'];
+    $project_nid = $values['project_nid'];
+
+    foreach ($file_upload as $file_tmp) {
+      $node_ids[] = $this->createNodeUniGImage($file_tmp, $project_nid);
     }
 
+    return $node_ids;
+  }
 
-    /**
-     * @param $file_temp
-     * @param $project_nid
-     * @return int
-     */
-    public function getFileId($file_temp, $project_nid)
-    {
-        $file_id = 0;
 
-        // Plupload
-        // ---------------------------------
-        // [tmppath] => 'temporary://o_hash.tmp',
-        // [tmpname] => 'o_hash.tmp',
-        // [name] => 'filename.jpg',
-        // [status] => 'done')
+  /**
+   * @param $file_temp
+   * @param $project_nid
+   *
+   * @return int
+   */
+  public function getFileId($file_temp, $project_nid) {
+    $file_id = 0;
 
-        $tmppath = $file_temp['tmppath'];
-        $tmpname = $file_temp['tmpname'];
-        $name = $file_temp['name'];
-        $status = $file_temp['status'];
+    // Plupload
+    // ---------------------------------
+    // [tmppath] => 'temporary://o_hash.tmp',
+    // [tmpname] => 'o_hash.tmp',
+    // [name] => 'filename.jpg',
+    // [status] => 'done')
 
-        $path_prefix_unig = '';
-        $path_destination = 'public://';
-        $path_unig = 'bilder/unig/';
-        $path_album = $path_prefix_unig . $project_nid . '/';
+    $tmppath = $file_temp['tmppath'];
+    $tmpname = $file_temp['tmpname'];
+    $name = $file_temp['name'];
+    $status = $file_temp['status'];
 
-        $this->checkProjectDir($path_destination, $path_unig, $path_album);
+    $path_prefix_unig = '';
+    $path_destination = 'public://';
+    $path_unig = 'unig/';
 
-        $uri_destination = $path_destination . $path_unig . $path_album . $name;
+    // If Pathauto is active, take aliasname from project for directory
+    $project_alias = \Drupal::service('path.alias_storage')->load(['source' => '/node/' . $project_nid]);
 
-        // Create file object from a locally copied file.
-        $uri = file_unmanaged_copy($tmppath, $uri_destination, FILE_EXISTS_REPLACE);
-        $file = File::Create([
-            'uri' => $uri,
-        ]);
+if($project_alias){
+  $project_name = $project_nid . '-' . $project_alias . '/';
+}
+else{
+  $project_name = $project_nid.'/';
 
-        $file->save();
+}
+    $path_album = $path_prefix_unig . $project_name;
 
-        $file_id = $file->id();
+    $this->checkProjectDir($path_destination, $path_unig, $path_album);
 
-        return $file_id;
-    }
+    $uri_destination = $path_destination . $path_unig . $path_album . $name;
+
+    // Create file object from a locally copied file.
+    $uri = file_unmanaged_copy($tmppath, $uri_destination, FILE_EXISTS_REPLACE);
+    $file = File::Create([
+      'uri' => $uri,
+    ]);
+
+    $file->save();
+
+    $file_id = $file->id();
+
+    return $file_id;
+  }
 
 
 }
