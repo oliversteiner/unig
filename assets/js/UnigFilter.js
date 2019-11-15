07931ca980c8a519d512c82a78511186cb62bf97
Oliver Steiner <oliver@mollo.ch>
2019-11-15 11:16:41 +0100

    add UnigFilter.js

--- assets/js/UnigFilter.js
+++ assets/js/UnigFilter.js
@@ -3,11 +3,53 @@
     version: '1.0.0',
     isToolbarOpen: false,
 
+    getId(event) {
+      const $elem = $(event.target).parents('.unig-filter-item');
+      return $elem.data('id');
+    },
+
+    update() {
+      console.log('Unig Filter Update');
+
+      // Remove Tag
+      $('.unig-filter-item-remove-trigger')
+        .once('unigFilter')
+        .click(event => {
+          const id = this.getId(event);
+          this.remove(id);
+        });
+    },
+
+    remove(id) {
+      console.log('Remove Tag with ID', id);
+      this.update();
+    },
+
+    addVisible() {
+      console.log('Add images to download list');
+    },
+
+    clearFilter() {
+      console.log('Clear Filter');
+    },
+
     constructor(context, settings) {
+      console.log('unigFilter');
+
       // Toolbar
       $('.unig-toolbar-filter-toggle-trigger', context).click(() => {
         this.isToolbarOpen = !this.isToolbarOpen;
         console.log('toggle filter', this.isToolbarOpen);
+      });
+
+      // add-visible-to-download-list
+      $('.unig-add-visible-to-download-list-trigger', context).click(() => {
+        this.addVisible();
+      });
+
+      // clear-filter
+      $('.unig-clear-filter-trigger', context).click(() => {
+        this.clearFilter();
       });
     },
 
@@ -18,6 +60,7 @@
         .once('unigFilter')
         .each(() => {
           this.constructor(context, settings);
+          this.update();
         });
     },
   };
